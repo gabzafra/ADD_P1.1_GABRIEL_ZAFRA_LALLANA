3d91adf91203543ac5771a3c893d9fba07931216
Gabriel Zafra <gabzafra@users.noreply.github.com>
2022-10-27 16:35:02 +0200

    created a logger for the login attemps

---
+++ ADD_P1.1_GABRIEL_ZAFRA_LALLANA/src/dam2/add/p11/Logger.java
@@ -0,0 +1,32 @@
+package dam2.add.p11;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Logger {
+  private File loggerFile;
+
+  public Logger(String loggerPath) {
+    this.loggerFile = new File(loggerPath);
+  }
+
+  public boolean addLog(String name, boolean isValid) {
+
+    try (BufferedWriter input = new BufferedWriter(new FileWriter(loggerFile, true))) {
+      Date now = new Date();
+      DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+
+      String formatedRow = dateFormat.format(now) + ";" + name + ";" + (isValid ? "OK" : "ERROR");
+
+      input.write(formatedRow);
+      input.newLine();
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+}
--- ADD_P1.1_GABRIEL_ZAFRA_LALLANA/src/dam2/add/p11/MainP11.java
+++ ADD_P1.1_GABRIEL_ZAFRA_LALLANA/src/dam2/add/p11/MainP11.java
@@ -1,5 +1,7 @@
 package dam2.add.p11;
 
+import java.util.Scanner;
+
 public class MainP11 {
 
   public static void main(String[] args) {
@@ -7,8 +9,12 @@ public class MainP11 {
         "D:/DAM/2022-2023/_repos/ADD_P1.1_GABRIEL_ZAFRA_LALLANA/ADD_P1.1_GABRIEL_ZAFRA_LALLANA/src/dam2/add/p11/files/acceso.txt";
     String tempFilePath =
         "D:/DAM/2022-2023/_repos/ADD_P1.1_GABRIEL_ZAFRA_LALLANA/ADD_P1.1_GABRIEL_ZAFRA_LALLANA/src/dam2/add/p11/files/temp.txt";
+    String logFilePath =
+        "D:/DAM/2022-2023/_repos/ADD_P1.1_GABRIEL_ZAFRA_LALLANA/ADD_P1.1_GABRIEL_ZAFRA_LALLANA/src/dam2/add/p11/files/login.log";
 
     Db db = new Db(usersFilePath, tempFilePath);
+
+    Logger logger = new Logger(logFilePath);
 
     UserHandler users = new UserHandler(db);
 
@@ -17,6 +23,12 @@ public class MainP11 {
 
     Usuario currentUser = users.loginUser(new Usuario("admin", "admin"));
 
+    logger.addLog("Trololo", false);
+
+    Scanner sc = new Scanner(System.in);
+    sc.nextLine();
+
+    System.out.println(logger.addLog("Trololo", true));
   }
 
 }
